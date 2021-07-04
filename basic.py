2ef0177e0af8e3184118cbe4e87ca416dd87fe7d
khan <khan@gmail.com>
2021-07-04 20:53:09 +0500

    'python_for_beginner'

---
+++ basic.py
@@ -0,0 +1,20 @@
+# Getting with basics of pyton 
+# Hello ,world program in python
+# By using print function we simply write a Hello ,World program
+print('Hello,World')
+# Data type in python
+# Type function in python
+print(type('Hello , World'))
+# How to find the length of string
+# len function in python use to find the length of itrator
+print(len("Hello World"))
+# variable
+# variable container that stor a data 
+var='Hello ,World'
+print(var)
+print(var * 3)
+
+# How to print multiple statement by using print() :
+
+print('My Name is Iqran Khan', 'I am student of virtual university of Pakistan' , 'I am a Software Engineer', sep=' . ' ,end='.')
+
